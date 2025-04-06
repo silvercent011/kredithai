51d7714d1f4ac3d3ba681de6e0fcd61bafd752f4
Evandro Jr <evandroojuniorr@hotmail.com>
2025-04-06 15:37:39 -0300

    Adding NavHostController in the parameters of the classes

--- app/src/main/java/com/example/kredithai/presentations/screens/HomeScreen.kt
+++ app/src/main/java/com/example/kredithai/presentations/screens/HomeScreen.kt
@@ -7,9 +7,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(navController: NavHostController) {
     Box(
         modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
     ) {
